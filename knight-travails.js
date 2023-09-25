56315885fa793ac2ac23ebc3d9eb249eecd8727c
Verbiturum <user@example.com>
2023-09-25 19:55:34 +0300

    Add functions knightMoves and printPath to find the moves and console.log them

--- knight-travails.js
+++ knight-travails.js
@@ -15,3 +15,41 @@ const possibleMoves = [
 function onBoard(pos) {
 	return pos[0] >= 0 && pos[0] < 8 && pos[1] >= 0 && pos[1] < 8;
 }
+
+function knightMoves(start, end) {
+
+	let queue = [new Node(start, 0)];
+
+	while (queue.length > 0) {
+		const node = queue.shift();
+		const pos = node.pos;
+
+		if (pos[0] === end[0] && pos[1] === end[1]) {
+			console.log(`Made it in ${node.moves} moves!`);
+			printPath(node);
+			return;
+		}
+
+		const moves = node.moves + 1;
+
+
+		for (const move of possibleMoves) {
+			const next = [pos[0] + move[0], pos[1] + move[1]];
+
+			if (onBoard(next)) {
+				let nextNode = new Node(next, moves);
+				nextNode.parent = node;
+				queue.push(nextNode);
+			}
+		}
+	}
+}
+
+function printPath(node) {
+	let path = [];
+	while (node) {
+		path.unshift(node.pos);
+		node = node.parent;
+	}
+	console.log(path);
+}
